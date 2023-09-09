0165abeaccfb6ef92f4bf92f6b61f5cb5d62fe13
kamalika0363 <59131714+doesnots@users.noreply.github.com>
2023-09-09 15:29:40 +0530

    session 2

--- Day2/questions.py
+++ Day2/questions.py
@@ -1,31 +1,31 @@
-# # LEVEL 1
-# # keep accepting elements from user till blank is given
-# # add all element in list
-# # print sum of all elements
+# LEVEL 1
+# keep accepting elements from user till blank is given
+# add all element in list
+# print sum of all elements
 
-# list = []
-# while True:
-#     data=input("Enter:")
-#     if data=="":
-#         break
-#     list.append(float(data))
-# print("Sum is:",sum(list))    
+list = []
+while True:
+    data=input("Enter:")
+    if data=="":
+        break
+    list.append(float(data))
+print("Sum is:",sum(list))    
 
-# # LEVEL 2
-# # keep accepting elements from user till blank is given
-# # add all element in list
-# # print sum of all elements
-# # print all elements > the average of all elements
+# LEVEL 2
+# keep accepting elements from user till blank is given
+# add all element in list
+# print sum of all elements
+# print all elements > the average of all elements
 
-# list = []
-# while True:
-#     data=input("Enter:")
-#     if data=="":
-#         break
-#     list.append(float(data))
-# avg=sum(list)/len(list)
-# for i in list:
-#     if i>avg:
-#         print(i)   
+list = []
+while True:
+    data=input("Enter:")
+    if data=="":
+        break
+    list.append(float(data))
+avg=sum(list)/len(list)
+for i in list:
+    if i>avg:
+        print(i)   
 
 
